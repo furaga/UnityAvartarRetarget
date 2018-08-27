98cdbb98f4de1962e73f3a92dcb24267daaaf2cb
furaga <furaga.fukahori@gmail.com>
2018-08-27 19:58:03 +0900

    Minor tunings

--- Assets/FAvatarRetarget/Scripts/Motion.cs
+++ Assets/FAvatarRetarget/Scripts/Motion.cs
@@ -30,21 +30,6 @@ public class Pose
     public int Frame;
     public List<Vector3> Positions = new List<Vector3> { };
 
-    // Bone
-    // 0: Trans
-    // 1: R ULeg
-    // 2: R Leg
-    // 3: L ULeg
-    // 4: L Leg
-    // 5: Spine
-    // 6: R Shoulder
-    // 7: R UArm
-    // 8: R Arm
-    // 9: L Shoulder
-    // 10: L UArm
-    // 11: L Arm
-    // 12: Neck
-
     public bool HasRotations = false;
     public List<float> BoneLengths = new List<float>();
     public List<Quaternion> Rotations = new List<Quaternion>();
@@ -63,6 +48,7 @@ public class Pose
     void updateBoneLengths()
     {
         // Posiitons are in the lsp order.
+        // 14 means hip
         int[] boneJoints = new int[]
         {
             2, 3,
@@ -70,7 +56,7 @@ public class Pose
             1, 0,
             3, 4,
             4, 5,
-            14, 12, // 14 is hip
+            14, 12,
             12, 8,
             8, 7,
             7, 6,
@@ -96,6 +82,21 @@ public class Pose
 
     void updateBoneRotations()
     {
+        // [Bone]
+        // 0: Trans
+        // 1: R ULeg
+        // 2: R Leg
+        // 3: L ULeg
+        // 4: L Leg
+        // 5: Spine
+        // 6: R Shoulder
+        // 7: R UArm
+        // 8: R Arm
+        // 9: L Shoulder
+        // 10: L UArm
+        // 11: L Arm
+        // 12: Neck
+
         for (int i = 0; i < 13; i++)
         {
             LocalRotations.Add(Quaternion.identity);
@@ -128,41 +129,7 @@ public class Pose
         LocalRotations[12] = calcRotation(Positions[13] - Positions[12], Positions[9] - Positions[8], rotLeftDown);
 
         Rotations = LocalRotations.ToList();
-
-        {
-            var positions = Positions.ToList();
-            positions.Add(hip);
-            int[] jointIdx = new int[] {
-                2, 3,
-                2, 1,
-                1, 0,
-                3, 4,
-                4, 5,
-                14, 12, // 14 is hip
-                12, 8,
-                8, 7,
-                7, 6,
-                12, 9,
-                9, 10,
-                10, 11,
-                12, 13,
-            };
-
-            //for (int i = 0; i < 13; i++)
-            //{
-            //    float axisLen = 0.15f;
-            //    var rot = Rotations[i];
-            //    var xAxis = rot * Vector3.right;
-            //    var yAxis = rot * Vector3.up;
-            //    var zAxis = rot * Vector3.forward;
-            //    Vector3 pos = (positions[jointIdx[2 * i]] + positions[jointIdx[2 * i + 1]]) * 0.5f;
-            //    Debug.DrawLine(pos, pos + xAxis * axisLen, Color.red);
-            //    Debug.DrawLine(pos, pos + yAxis * axisLen, Color.green);
-            //    Debug.DrawLine(pos, pos + zAxis * axisLen, Color.blue);
-            //}
-        }
-
-
+        
 
         // Convert Global Rotations to Local Rotations
         var invRoot = Quaternion.Inverse(LocalRotations[0]);
@@ -214,25 +181,13 @@ public class Pose
 
         int[] joints = new int[]
         {
-            2, 3,
-            1, 0,
-            4, 5,
-            12,
-            8, 7, 6,
-            9, 10, 11,
-            13,
+            2, 3, 1, 0, 4, 5, 12, 8, 7, 6, 9, 10, 11, 13,
         };
 
-        // 14 is hip
+        // 14 means hip
         int[] parentJoints = new int[]
         {
-            14, 14,
-            2, 1,
-            3, 4,
-            14,
-            12, 8, 7,
-            12, 9, 10,
-            12,
+            14, 14, 2, 1, 3, 4, 14, 12, 8, 7, 12, 9, 10, 12,
         };
 
         // fk results = joint positions
